5b6fdcf5b87d954d1a998d39fcaabc708b1a33ec
hjkim <guswns589898@gmail.com>
2022-11-15 11:20:17 +0900

    Shapes Matching

---
+++ GeometryComparison_HuMoment/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace GeometryComparison_HuMoment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Run
+            // 
+            this.btn_Run.Location = new System.Drawing.Point(285, 160);
+            this.btn_Run.Name = "btn_Run";
+            this.btn_Run.Size = new System.Drawing.Size(160, 93);
+            this.btn_Run.TabIndex = 0;
+            this.btn_Run.Text = "button1";
+            this.btn_Run.UseVisualStyleBackColor = true;
+            this.btn_Run.Click += new System.EventHandler(this.btn_Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 531);
+            this.Controls.Add(this.btn_Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Run;
+    }
+}
+
---
+++ GeometryComparison_HuMoment/Form1.cs
@@ -0,0 +1,107 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Point = OpenCvSharp.Point;
+
+namespace GeometryComparison_HuMoment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private (Mat, Point[][], HierarchyIndex[]) FindContours(Mat mat)
+        {
+            Cv2.CvtColor(mat, mat, ColorConversionCodes.BGR2GRAY);
+
+            Cv2.Threshold(mat, mat, thresh: 87, maxval: 255, ThresholdTypes.BinaryInv);
+
+            Cv2.FindContours(mat, out Point[][] contours, out HierarchyIndex[] hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+            Mat edge = new Mat();
+            Cv2.Canny(mat, edge, threshold1: 96, threshold2: 96);
+
+            return (edge, contours, hierarchy);
+        }
+
+        private void btn_Run_Click(object sender, EventArgs e)
+        {
+            Point[][] contours1;
+            HierarchyIndex[] hierarchy1;
+
+            Mat pattern;
+            (pattern, contours1, hierarchy1) = FindContours(BitmapConverter.ToMat(Properties.Resources.pattern_nut));
+
+            Cv2.ImShow("pattern", pattern);
+
+            Point[][] contours2;
+            HierarchyIndex[] hierarchy2;
+
+            Mat data;
+            (data, contours2, hierarchy2) = FindContours(BitmapConverter.ToMat(Properties.Resources.data));
+
+            var output = new Mat();
+            Cv2.CvtColor(data, output, ColorConversionCodes.GRAY2BGR);
+
+            Cv2.ImShow("data", data);
+
+            for (int i = 0; i < contours2.GetLength(0); ++i)
+            {
+                var area = Cv2.ContourArea(contours2[i].ToArray());
+
+                if (area < 100)
+                {
+                    var rect = Cv2.BoundingRect(contours2[i]);
+                    rect.Inflate(width: 1, height: 1);
+                    output.Rectangle(rect, Scalar.Black, thickness: -1);
+                }
+            }
+
+            for (int i = 0; i < contours2.GetLength(0); ++i)
+            {
+                var area = Cv2.ContourArea(contours2[i].ToArray());
+
+                if (area < 100)
+                {
+                    continue;
+                }
+
+                var rect = Cv2.BoundingRect(contours2[i]);
+
+                var moments = Cv2.Moments(contours2[i]);
+                Point centroid = new Point();
+                centroid.X = (int)(moments.M10 / moments.M00);
+                centroid.Y = (int)(moments.M01 / moments.M00);
+                //output.Circle(centroid, radius: 3, Scalar.Yellow, thickness: -1);
+
+                double ratio = Cv2.MatchShapes(data[rect], pattern, ShapeMatchModes.I1);
+                if(ratio < 0.01)
+                {
+                    output.PutText(ratio.ToString("F3"), rect.TopLeft, HersheyFonts.HersheySimplex, fontScale: 0.8, Scalar.White);
+                    output.Rectangle(rect, Scalar.GreenYellow);
+                }
+                //else
+                //{
+                //    output.PutText(ratio.ToString("F3"), rect.TopLeft, HersheyFonts.HersheySimplex, fontScale: 0.8, Scalar.Red);
+                //    output.Rectangle(rect, Scalar.GreenYellow);
+                //}
+
+                Cv2.MinEnclosingCircle(contours2[i], out Point2f center, out float fadius);
+                var diff = center - centroid;
+                double distance = Math.Sqrt(diff.X * diff.X + diff.Y * diff.Y);
+                //output.PutText(distance.ToString("F1"), centroid + new Point(10, 0), HersheyFonts.HersheySimplex, fontScale: 0.8, Scalar.Yellow, thickness: 2);
+            }
+
+            Cv2.ImShow("output", output);
+        }
+    }
+}
