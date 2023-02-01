d9fa9b42711ec675f035db11e1317b7f6df057ac
hjkim <guswns589898@gmail.com>
2023-02-01 16:26:21 +0900

    Contours 윤곽선 검출

---
+++ ContourSample/Form1.cs
@@ -0,0 +1,49 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Point = OpenCvSharp.Point;
+
+namespace ContourSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ImageProcessing();
+        }
+
+        public void ImageProcessing()
+        {
+            Mat src = BitmapConverter.ToMat(Properties.Resources.Image);
+            Mat src1 = new Mat(), src2 = new Mat();
+            src.CopyTo(src1);
+            src.CopyTo(src2);
+            Cv2.ImShow("scr", src);
+
+            Mat bin = new Mat();
+            Cv2.CvtColor(src, bin, ColorConversionCodes.BGR2GRAY);
+
+            Cv2.Threshold(bin, bin, 127, 255, ThresholdTypes.Binary);
+
+            Mat hierarchy1 = new Mat();
+            Cv2.FindContours(bin, out Mat[] contour1, hierarchy1,
+                RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+
+            for (int i = 0; i < contour1.Length; i++)
+            {
+                Cv2.DrawContours(src1, contour1, i, Scalar.Red, 1, LineTypes.AntiAlias);
+            }
+            Cv2.ImShow("1", src1);
+        }
+    }
+}
